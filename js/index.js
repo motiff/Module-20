60924551a0e0b06eca59b543ec8d4a0d04433d76
tiffanywu404 <145884590+tiffanywu404@users.noreply.github.com>
2024-04-05 01:05:34 -0700

    image, stripe, some text

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// window.addEventListener("scroll", function() {
+//     var header = document.getElementById("jumbotron");
+//       if ((window.scrollY) > (jumbotron.offsetTop + jumbotron.offsetHeight)) {
+//         // $(".sidebar").addClass("sticky-top");
+//           $(".sidebar").css("position", "sticky");
+//         //   $(".sidebar").css("width", "calc(((100% - 260px)/6) + 20px)");
+//       }
+//       else {
+//         // $(".sidebar").removeClass("sticky-top");
+//           $(".sidebar").css("position", "static");
+//         //   $(".sidebar").css("width", "auto");
+//       }
+//     });
